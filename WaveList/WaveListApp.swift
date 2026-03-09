bdd04ecc3674774d1427b126ccab8cc60d4ddb77
yazan <yazankh0115@gmail.com>
2026-03-10 00:12:34 +0100

    Initial Commit

---
+++ WaveList/WaveListApp.swift
@@ -0,0 +1,32 @@
+//
+//  WaveListApp.swift
+//  WaveList
+//
+//  Created by Yazan Khraiba on 2026-03-10.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WaveListApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ WaveListTests/WaveListTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaveListTests.swift
+//  WaveListTests
+//
+//  Created by Yazan Khraiba on 2026-03-10.
+//
+
+import Testing
+@testable import WaveList
+
+struct WaveListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
